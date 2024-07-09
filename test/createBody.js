7015cc8454d8ea22ddde83f56a7cfc87cbfc45e3
Adam Boretos <aboretos@rapid7.com>
2024-07-09 20:57:24 +0000

    tweaked typo in createBody tests

--- test/createBody.js
+++ test/createBody.js
@@ -2,18 +2,18 @@
 const test = require('tap').test;
 const dimensions = 2;
 
-test('can debug setters', function (t) {
+test('can debug setters in Body', function (t) {
   const Body = require('../lib/codeGenerators/generateCreateBody')(dimensions, true);
   const b = new Body();
   t.throws(() => b.pos.x = 'foo', /Cannot set non-numbers to x/);
-  // t.throws(() => b.pos.y = 'foo', /Cannot set non-numbers to x/);
+  t.throws(() => b.pos.y = 'foo', /Cannot set non-numbers to y/);
   t.end();
 });
 
-test('can create appropriate debug setter code', function (t) {
+test('can debug setters in Vector', function (t) {
   const Vector = require('../lib/codeGenerators/generateCreateBody').generateCreateVectorFunction(dimensions, true);
   const v = new Vector();
   t.throws(() => v.x = 'foo', /Cannot set non-numbers to x/);
-  // t.throws(() => v.y = 'foo', /Cannot set non-numbers to x/);
+  t.throws(() => v.y = 'foo', /Cannot set non-numbers to y/);
   t.end();
 })
