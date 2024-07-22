b9e29e65a9adac783e0e879e64665160c2a143d2
Adam Boretos <aboretos@rapid7.com>
2024-07-22 19:14:08 +0000

    fixed springForce.js

--- test/springForce.js
+++ test/springForce.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-shadow */
-var test = require('tap').test;
+const test = require('tap').test;
 
-var dimensions = 2;
-var createSpringForce = require('../lib/codeGenerators/generateCreateSpringForce')(dimensions);
-var Body = require('../lib/codeGenerators/generateCreateBody')(dimensions);
-var Spring = require('../lib/spring');
-var random = require('ngraph.random')(42);
+const dimensions = 2;
+const createSpringForce = require('../lib/codeGenerators/generateCreateSpringForce')(dimensions);
+const Body = require('../lib/codeGenerators/generateCreateBody')(dimensions);
+const Spring = require('../lib/spring');
+const random = require('ngraph.random')(42);
 
 test('Initialized with default value', function (t) {
   t.throws(() => createSpringForce());
@@ -14,13 +14,13 @@ test('Initialized with default value', function (t) {
 
 
 test('Should bump bodies at same position', function (t) { 
-  var body1 = new Body(0, 0);
-  var body2 = new Body(0, 0);
+  const body1 = new Body(0, 0);
+  const body2 = new Body(0, 0);
   // length between two bodies is 2, while ideal length is 1. Each body
   // should start moving towards each other after force update
-  var idealLength = 1;
-  var spring = new Spring(body1, body2, idealLength);
-  var springForce = createSpringForce({springCoefficient: 0.1, springLength: 1}, random);
+  const idealLength = 1;
+  const spring = new Spring(body1, body2, idealLength);
+  const springForce = createSpringForce({springCoefficient: 0.1, springLength: 1}, random);
   springForce.update(spring);
 
   t.ok(body1.force.x > 0, 'Body 1 should go right');
@@ -29,15 +29,15 @@ test('Should bump bodies at same position', function (t) {
 });
 
 test('Check spring force direction', function (t) {
-  var springForce = createSpringForce({springCoefficient: 0.1, springLength: 1});
+  const springForce = createSpringForce({springCoefficient: 0.1, springLength: 1});
 
   t.test('Should contract two bodies when ideal length is smaller than actual', function (t) { 
-    var body1 = new Body(-1, 0);
-    var body2 = new Body(+1, 0);
+    const body1 = new Body(-1, 0);
+    const body2 = new Body(+1, 0);
     // length between two bodies is 2, while ideal length is 1. Each body
     // should start moving towards each other after force update
-    var idealLength = 1;
-    var spring = new Spring(body1, body2, idealLength);
+    const idealLength = 1;
+    const spring = new Spring(body1, body2, idealLength);
     springForce.update(spring);
 
     t.ok(body1.force.x > 0, 'Body 1 should go right');
@@ -46,12 +46,12 @@ test('Check spring force direction', function (t) {
   });
 
   t.test('Should repel two bodies when ideal length is larger than actual', function (t) { 
-    var body1 = new Body(-1, 0);
-    var body2 = new Body(+1, 0);
+    const body1 = new Body(-1, 0);
+    const body2 = new Body(+1, 0);
     // length between two bodies is 2, while ideal length is 1. Each body
     // should start moving towards each other after force update
-    var idealLength = 3;
-    var spring = new Spring(body1, body2, idealLength);
+    const idealLength = 3;
+    const spring = new Spring(body1, body2, idealLength);
     springForce.update(spring);
 
     t.ok(body1.force.x < 0, 'Body 1 should go left');
